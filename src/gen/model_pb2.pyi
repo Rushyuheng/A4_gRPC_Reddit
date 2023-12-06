bf80ce64023fc70e91b5ab4ea5d1d816e504c6c1
YuHeng Huang <jamesyhh@gmail.com>
2023-12-06 16:01:12 -0500

    generate python file from protobuf

---
+++ src/gen/model_pb2.pyi
@@ -0,0 +1,61 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class User(_message.Message):
+    __slots__ = ["user_id"]
+    USER_ID_FIELD_NUMBER: _ClassVar[int]
+    user_id: str
+    def __init__(self, user_id: _Optional[str] = ...) -> None: ...
+
+class Post(_message.Message):
+    __slots__ = ["title", "text", "video_url", "image_url", "author", "score", "state", "publication_date"]
+    class PostState(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+        POST_STATE_NORMAL: _ClassVar[Post.PostState]
+        POST_STATE_LOCKED: _ClassVar[Post.PostState]
+        POST_STATE_HIDDEN: _ClassVar[Post.PostState]
+    POST_STATE_NORMAL: Post.PostState
+    POST_STATE_LOCKED: Post.PostState
+    POST_STATE_HIDDEN: Post.PostState
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    TEXT_FIELD_NUMBER: _ClassVar[int]
+    VIDEO_URL_FIELD_NUMBER: _ClassVar[int]
+    IMAGE_URL_FIELD_NUMBER: _ClassVar[int]
+    AUTHOR_FIELD_NUMBER: _ClassVar[int]
+    SCORE_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    PUBLICATION_DATE_FIELD_NUMBER: _ClassVar[int]
+    title: str
+    text: str
+    video_url: str
+    image_url: str
+    author: User
+    score: int
+    state: Post.PostState
+    publication_date: _timestamp_pb2.Timestamp
+    def __init__(self, title: _Optional[str] = ..., text: _Optional[str] = ..., video_url: _Optional[str] = ..., image_url: _Optional[str] = ..., author: _Optional[_Union[User, _Mapping]] = ..., score: _Optional[int] = ..., state: _Optional[_Union[Post.PostState, str]] = ..., publication_date: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+
+class Comment(_message.Message):
+    __slots__ = ["text", "author", "score", "state", "publication_date"]
+    class CommetState(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+        POST_STATE_NORMAL: _ClassVar[Comment.CommetState]
+        POST_STATE_HIDDEN: _ClassVar[Comment.CommetState]
+    POST_STATE_NORMAL: Comment.CommetState
+    POST_STATE_HIDDEN: Comment.CommetState
+    TEXT_FIELD_NUMBER: _ClassVar[int]
+    AUTHOR_FIELD_NUMBER: _ClassVar[int]
+    SCORE_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    PUBLICATION_DATE_FIELD_NUMBER: _ClassVar[int]
+    text: str
+    author: User
+    score: int
+    state: Comment.CommetState
+    publication_date: _timestamp_pb2.Timestamp
+    def __init__(self, text: _Optional[str] = ..., author: _Optional[_Union[User, _Mapping]] = ..., score: _Optional[int] = ..., state: _Optional[_Union[Comment.CommetState, str]] = ..., publication_date: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
---
+++ src/gen/service_pb2.pyi
@@ -0,0 +1,109 @@
+import model_pb2 as _model_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CreatePostRequest(_message.Message):
+    __slots__ = ["post"]
+    POST_FIELD_NUMBER: _ClassVar[int]
+    post: _model_pb2.Post
+    def __init__(self, post: _Optional[_Union[_model_pb2.Post, _Mapping]] = ...) -> None: ...
+
+class CreatePostRespond(_message.Message):
+    __slots__ = ["post"]
+    POST_FIELD_NUMBER: _ClassVar[int]
+    post: _model_pb2.Post
+    def __init__(self, post: _Optional[_Union[_model_pb2.Post, _Mapping]] = ...) -> None: ...
+
+class VotePostRequest(_message.Message):
+    __slots__ = ["post_id", "vote"]
+    POST_ID_FIELD_NUMBER: _ClassVar[int]
+    VOTE_FIELD_NUMBER: _ClassVar[int]
+    post_id: int
+    vote: bool
+    def __init__(self, post_id: _Optional[int] = ..., vote: bool = ...) -> None: ...
+
+class VotePostRespond(_message.Message):
+    __slots__ = ["post"]
+    POST_FIELD_NUMBER: _ClassVar[int]
+    post: _model_pb2.Post
+    def __init__(self, post: _Optional[_Union[_model_pb2.Post, _Mapping]] = ...) -> None: ...
+
+class GetPostRequest(_message.Message):
+    __slots__ = ["post_id"]
+    POST_ID_FIELD_NUMBER: _ClassVar[int]
+    post_id: int
+    def __init__(self, post_id: _Optional[int] = ...) -> None: ...
+
+class GetPostRespond(_message.Message):
+    __slots__ = ["post"]
+    POST_FIELD_NUMBER: _ClassVar[int]
+    post: _model_pb2.Post
+    def __init__(self, post: _Optional[_Union[_model_pb2.Post, _Mapping]] = ...) -> None: ...
+
+class CreateCommentRequest(_message.Message):
+    __slots__ = ["comment"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ...) -> None: ...
+
+class CreateCommentRespond(_message.Message):
+    __slots__ = ["comment"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ...) -> None: ...
+
+class VoteCommentRequest(_message.Message):
+    __slots__ = ["comment_id", "vote"]
+    COMMENT_ID_FIELD_NUMBER: _ClassVar[int]
+    VOTE_FIELD_NUMBER: _ClassVar[int]
+    comment_id: int
+    vote: bool
+    def __init__(self, comment_id: _Optional[int] = ..., vote: bool = ...) -> None: ...
+
+class VoteCommentRespond(_message.Message):
+    __slots__ = ["comment"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ...) -> None: ...
+
+class MostUpvoteCommentRequest(_message.Message):
+    __slots__ = ["mostN"]
+    MOSTN_FIELD_NUMBER: _ClassVar[int]
+    mostN: int
+    def __init__(self, mostN: _Optional[int] = ...) -> None: ...
+
+class MostUpvoteCommentRespond(_message.Message):
+    __slots__ = ["comment", "has_replies"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    HAS_REPLIES_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    has_replies: bool
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ..., has_replies: bool = ...) -> None: ...
+
+class ExpandReplyRequest(_message.Message):
+    __slots__ = ["comment_id", "mostN"]
+    COMMENT_ID_FIELD_NUMBER: _ClassVar[int]
+    MOSTN_FIELD_NUMBER: _ClassVar[int]
+    comment_id: int
+    mostN: int
+    def __init__(self, comment_id: _Optional[int] = ..., mostN: _Optional[int] = ...) -> None: ...
+
+class ExpandReplyRespond(_message.Message):
+    __slots__ = ["comment", "reply"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    REPLY_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    reply: _containers.RepeatedCompositeFieldContainer[CommentWithReply]
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ..., reply: _Optional[_Iterable[_Union[CommentWithReply, _Mapping]]] = ...) -> None: ...
+
+class CommentWithReply(_message.Message):
+    __slots__ = ["comment", "reply"]
+    COMMENT_FIELD_NUMBER: _ClassVar[int]
+    REPLY_FIELD_NUMBER: _ClassVar[int]
+    comment: _model_pb2.Comment
+    reply: _containers.RepeatedCompositeFieldContainer[_model_pb2.Comment]
+    def __init__(self, comment: _Optional[_Union[_model_pb2.Comment, _Mapping]] = ..., reply: _Optional[_Iterable[_Union[_model_pb2.Comment, _Mapping]]] = ...) -> None: ...
